988e260f53628326690a8f4c7ebc8d73e9cd7508
luiztools <luizfduartejr@gmail.com>
2022-06-07 23:30:55 -0300

    aula 3

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-//BTCBUSD
-//36291
-//35764
+const axios = require("axios");
+const api = require("imersao-bot-cripto-api");
+
+const credentials = {
+    apiKey: "<sua api key>",
+    apiSecret: "<seu api secret>",
+    test: true
+}
 
 function calcRSI(closes) {
     let gains = 0;
@@ -18,22 +23,28 @@ function calcRSI(closes) {
     return 100 - (100 / (1 + strength));
 }
 
-let bought = false;
+let bought = true;
 
 async function process() {
-    const axios = require("axios");
-    const response = await axios.get("https://api.binance.com/api/v3/klines?symbol=BTCBUSD&interval=1m");
+    const symbol = "BTCBUSD";
+    const quantity = 0.001;
+
+    const response = await axios.get(`https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=1m`);
 
     const closes = response.data.map(candle => parseFloat(candle[4]));
     const rsi = calcRSI(closes);
     console.log(rsi);
 
-    if (rsi > 70 && bought){
+    if (rsi > 70 && bought) {
         console.log("Sobrecomprado!");
+        const sellResult = await api.sell(credentials, symbol, quantity);
+        console.log(sellResult);
         bought = false;
     }
-    else if (rsi < 30 && !bought){
+    else if (rsi < 30 && !bought) {
         console.log("Sobrevendido!");
+        const buyResult = await api.buy(credentials, symbol, quantity);
+        console.log(buyResult);
         bought = true;
     }
 }
